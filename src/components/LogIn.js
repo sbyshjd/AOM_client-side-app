b8eea96e085746b28d5e60e731965071c0a95103
Boyuan Shi <sbyshjd@hotmail.com>
2020-05-29 15:12:27 +0200

    finish the style for singup and login

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './login.css';
+import { Link } from 'react-router-dom';
 
 class LogIn extends Component {
     render() {
@@ -9,20 +10,34 @@ class LogIn extends Component {
               <div className="card o-hidden border-0 shadow-lg p-0">
                <div className='card-body p-0'>
                 <div className="row">
-                     <div className='col-5 bg-image rounded-left rounded-lg d-none d-md-block' >
-                     </div>
-                     <div className='col-7'>
-                      <p>Log in your Account</p>
-                      <h1>aaa</h1>
-                      <h1>aaa</h1>
-                      <h1>aaa</h1>
-                      <h1>aaa</h1>
-                      <h1>aaa</h1>
-                      <h1>aaa</h1>
-                      <h1>aaa</h1>
-                      <h1>aaa</h1>
-                      <h1>aaa</h1>
-                     </div>
+                    <div className='col-5 bg-image rounded-left rounded-lg d-none d-md-block' >
+                    </div>
+                    <div className='col-12 col-md-7 pl-5 pl-md-5 pt-5 pb-3 pr-5'>
+                        <form className='text-center'>
+                            <h3>Login your Account</h3>
+                            <div className='form-group mt-4'>
+                                <input type="text" placeholder='Username' className='form-control pt-4 pb-4 rounded-pill text-muted'/>
+                            </div>
+                            <div className='form-group mt-2'>
+                                <input type="text" placeholder='Email Adress' className='form-control pt-4 pb-4 rounded-pill text-muted'/>
+                            </div>
+                            <div className='row'>
+                                <div className='form-group col-6'>
+                                    <input type="text" placeholder='Password' className='form-control pt-4 pb-4 rounded-pill text-muted'/>
+                                </div>
+                                <div className='form-group col-6'>
+                                    <input type="text" placeholder='Repeat password' className='form-control pt-4 pb-4 rounded-pill text-muted'/>
+                                </div>
+                            </div>
+                            <Link to='/' type='submit' className='btn btn-block btn-primary pt-2 pb-2 rounded-pill'>Register</Link>
+                        </form>
+                        <hr/>
+                        <Link to='/' type='submit' className='btn btn-block btn-danger pt-2 pb-2 rounded-pill'>Register with Google</Link>
+                        <Link to='/' type='submit' className='btn btn-block btn-info pt-2 pb-2 rounded-pill'>Register with Facebook</Link>
+                        <hr/>
+                        <Link to='/login' className='d-block text-center text-primary mt-3' style={{fontSize:'0.8rem'}}>Forgot your password?</Link>
+                        <Link to='/login' className='d-block text-center text-primary mb-3' style={{fontSize:'0.8rem'}}>Arleady have an account? Log in!</Link>
+                    </div>
                 </div>
                </div>     
               </div>
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './signup.css'
+import { Link } from 'react-router-dom';
 
 class SignUp extends Component {
     render() {
@@ -8,24 +9,35 @@ class SignUp extends Component {
              <div className='container' >
               <div className="card o-hidden border-0 shadow-lg p-0">
                <div className='card-body p-0'>
-                <div className="row">
-                     <div className='col-5 bg-image rounded-left d-none d-md-block' >
-                     </div>
-                     <div className='col-7'>
-                     <form className='text-center'>
-                     <p>Log in your Account</p>
-                      <h1>aaa</h1>
-                      <h1>aaa</h1>
-                      <h1>aaa</h1>
-                      <h1>aaa</h1>
-                      <h1>aaa</h1>
-                      <h1>aaa</h1>
-                      <h1>aaa</h1>
-                      <h1>aaa</h1>
-                      <h1>aaa</h1>
-                     </form>
-                        
-                     </div>
+                <div className="row pr-3">
+                    <div className='col-5 bg-image rounded-left d-none d-md-block' >
+                    </div>
+                    <div className='col-12 col-md-7 pl-5 pl-md-5 pt-5 pb-3 pr-5'>
+                        <form className='text-center'>
+                            <h3>Create an Account</h3>
+                            <div className='form-group mt-4'>
+                                <input type="text" placeholder='Username' className='form-control pt-4 pb-4 rounded-pill text-muted'/>
+                            </div>
+                            <div className='form-group mt-2'>
+                                <input type="text" placeholder='Email Adress' className='form-control pt-4 pb-4 rounded-pill text-muted'/>
+                            </div>
+                            <div className='row'>
+                                <div className='form-group col-6'>
+                                    <input type="text" placeholder='Password' className='form-control pt-4 pb-4 rounded-pill text-muted'/>
+                                </div>
+                                <div className='form-group col-6'>
+                                    <input type="text" placeholder='Repeat password' className='form-control pt-4 pb-4 rounded-pill text-muted'/>
+                                </div>
+                            </div>
+                            <Link to='/' type='submit' className='btn btn-block btn-primary pt-2 pb-2 rounded-pill'>Register</Link>
+                        </form>
+                        <hr/>
+                        <Link to='/' type='submit' className='btn btn-block btn-danger pt-2 pb-2 rounded-pill'>Register with Google</Link>
+                        <Link to='/' type='submit' className='btn btn-block btn-info pt-2 pb-2 rounded-pill'>Register with Facebook</Link>
+                        <hr/>
+                        <Link to='/login' className='d-block text-center text-primary mt-3' style={{fontSize:'0.8rem'}}>Forgot your password?</Link>
+                        <Link to='/login' className='d-block text-center text-primary mb-3' style={{fontSize:'0.8rem'}}>Arleady have an account? Log in!</Link>
+                    </div>
                 </div>
                </div>     
               </div>
